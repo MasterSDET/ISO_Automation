58904cb535f74f97cf9390f7db657afbf26735b8
mohitgaur <mohitgaur@192.168.0.106>
2022-10-07 22:28:59 +0530

    base file ios

--- src/main/java/IOS_Autmomation_Base_File.java
+++ src/main/java/IOS_Autmomation_Base_File.java
@@ -19,105 +19,36 @@ import org.testng.annotations.Test;
 import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.android.options.UiAutomator2Options;
-    
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.options.XCUITestOptions;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
  
     
     public class IOS_Autmomation_Base_File
 {
-public static AndroidDriver driver;
+public static IOSDriver driver;
 
 @BeforeClass
 public void IOS_Base_File () throws MalformedURLException
 {		
 	
+	XCUITestOptions options = new XCUITestOptions();
+	options.setDeviceName("Iphone 14 pro");
+	options.setApp("");
+	options.setPlatformVersion("");
+	options.setWdaLaunchTimeout(Duration.ofSeconds(20));
 	
-	UiAutomator2Options options = new UiAutomator2Options();
-	options.setDeviceName("Test_Device2");
-	options.setUdid("RZ8R22L5RQV");
-	options.setPlatformName("Android");
-	options.setPlatformVersion("12");
-	options.setChromedriverExecutable("//Users//mohitgaur//Downloads//chromedriver");
+	 driver =  new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub/"), options);
+
+ /* 
+
 	options.setAppPackage("com.androidsample.generalstore");
 	options.setAppActivity("com.androidsample.generalstore.SplashActivity");
 	
-
-	 driver =  new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub/"), options);
-
-
-}
-
-public static void swipeaction(WebElement ele, String direction)
-{
+*/
 	
-((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
-		    
-			"elementId", ((RemoteWebElement)ele).getId(),
-		    "direction", "left",			    
-		    "percent", 0.75
-		));
-}
-public static void Dragactivites(WebElement Dra, String direction)
-{
-	((JavascriptExecutor) driver).executeScript("mobile: dragGesture", ImmutableMap.of(
-		    "elementId", ((RemoteWebElement) Dra).getId(),
-		    "endX", 619,
-		    "endY", 560
-		));
-}
-public static void wait_Condition(WebElement put_wait, String direction)
-{
-	// WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
- //   wait.until(ExpectedConditions.visibilityOf(wait(10)));
-}
 
-public static void longPressAction(WebElement LongPress)
-{
-	((JavascriptExecutor)driver).executeScript("mobile: longClickGesture",
-			ImmutableMap.of("elementId",((RemoteWebElement) LongPress).getId(),
-					"duration",2000));
-}
-
-public static void scrollToEndAction()
-{
-	boolean canScrollMore;
-	do
-	{
-	 canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
-		    
-		));
-	}while(canScrollMore);
-}
-
-
-public static void swipeAction(WebElement ele,String direction)
-{
-	((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
-			"elementId", ((RemoteWebElement)ele).getId(),
-		 
-		    "direction", direction,
-		    "percent", 0.75
-		));
-	
-	
-}
-
-
-public static Double getFormattedAmount(String amount)
-{
-	Double price = Double.parseDouble(amount.substring(1));
-	return price;
-	
-}
-@AfterClass
-public void tearDown()
-{
-	driver.quit();
- 
-    
-	}
-
-	
+}	
 }
 
     
